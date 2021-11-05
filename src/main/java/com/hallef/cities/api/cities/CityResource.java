d9c9bf108dce6af4b2a1f0580615493c3e87a78e
halleflima <halleflima@hotmail.com>
2021-11-04 23:00:40 -0300

    Adicionada ficture de pesquisa por ID em cidades

--- src/main/java/com/hallef/cities/api/cities/CityResource.java
+++ src/main/java/com/hallef/cities/api/cities/CityResource.java
@@ -2,9 +2,13 @@ package com.hallef.cities.api.cities;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("cities")
@@ -20,5 +24,16 @@ public class CityResource {
     @GetMapping
     public Page<City> cities(final Pageable page) {
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id){
+        Optional<City> optional = repository.findById(id);
+
+        if (optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
